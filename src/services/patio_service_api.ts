01b3b1e48b88e22f621526455c89a3f218365d40
rofviv <royvillarroel94@gmail.com>
2024-11-29 17:03:33 -0400

    logs remove user

--- src/services/patio_service_api.ts
+++ src/services/patio_service_api.ts
@@ -31,10 +31,8 @@ class PatioServiceApi {
           },
         }
       );
-      console.log("response getUser: ", response.data.data);
       return response.data.data;
     } catch (error) {
-      console.error("Error in getUser:", error);
       return null;
     }
   }
