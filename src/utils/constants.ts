c17064c61426d4494b41e183d1432bb1b05c148a
rofviv <royvillarroel94@gmail.com>
2024-12-06 15:04:08 -0400

    instructions readme

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -2,9 +2,9 @@ export default class Constants {
   static merchantDefaultId = 31970;
   static clientMerchantId = 3536;
   static vehicleType_motorcycle = 2;
-  static providerName = "HOLA-PATIO";
+  static providerName = "CHATBOT";
   static cityId_SC = 1;
   static providerImage =
     "https://storage-patiodriver.s3.amazonaws.com/holapatio-logo.webp";
-  static delayMessage = 1200;
+  static delayMessage = 2000;
 }
